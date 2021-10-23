4a8fd0129372eb15c94c4503aadf7c41ba81ef9a
phanthianhthu <thuptagcc19106@fpt.edu.vn>
2021-10-23 09:23:38 +0700

    update.

--- webdesign/connection.php
+++ webdesign/connection.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://jaayojojmjkvzn:84db5e35b35e0672c84ba4786a089db76f005a0c931490ee91a13d947fa3fda1@ec2-44-195-240-222.compute-1.amazonaws.com:5432/d6771km0h01h60");
+	$Connect = pg_connect("postgres://sqrzjnwkaiwhoo:b4f2f1e5b61b3c03e503fdaee1119f4af484bf6050f93db427611523b3b80765@ec2-44-198-215-235.compute-1.amazonaws.com:5432/dajilnpgjps16i");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
